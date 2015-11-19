ec5570645ef5826e5cbe34110e3dbaae5ad47cd6
Lurkon <rlayneorr@aim.com>
2015-11-19 16:20:06 -0500

    Command Line Arguments dealt with, pending response from malevolent overlord

--- udpclient.c
+++ udpclient.c
@@ -27,6 +27,9 @@ int abortsignal = 0;
 //buffer size
 #define MAX 300
 
+//Which protocol are we using?
+int protocol = 0;
+
 /* The plan is to have three major parts to this program:
  *	connection: connect to the proxy
  *	draw: draw the polygon
@@ -39,6 +42,7 @@ int main (int argc, char **argv)
 	char *serverName;
 	int serverPort;
 	int numSides;
+	int sideLength;
 
 	//UDP Declarations
 	int sock;
@@ -51,10 +55,10 @@ int main (int argc, char **argv)
 	int respStringLen;
 
 	//check command line parameters
-	if (argc != 4)
+	if (argc != 5)
 	{
 		printf("Usage: ./udpclient.c \"serverName\" \"serverPort\"
-			\"numberOfSides\"");
+			\"numberOfSides\" \"sideLength\"\n");
 		return 0;
 	}
 
@@ -63,19 +67,45 @@ int main (int argc, char **argv)
 	sscanf(argv[2],"%d",&serverPort);
 	servPort = serverPort;
 	sscanf(argv[3],"%d",&numSides);
+	sscanf(argv[4],"%d",&sideLength);
+	if (serverPort == -1)
+	{
+		printf("Invalid Server Port.\n");
+		return 0;
+	}
+	if (numSides<4||numSides>8)
+	{
+		printf("Invalid number of sides.\n");
+		return 0;
+	}
+	if (sideLength<1)
+	{
+		printf("Invalid side length.\n");
+	}
+
+	//make connection
+	//Try to connect using Group 9 Protocol!
+	if()
+	{
+	}
+	else
+	{
+		//Try to connect using Class Protocol!
+	}
 }
 
-void draw (int n) {
+void draw (int N, int L)
+{
 	//int i
-	//for (i=0; i<n; i++
+	//for (i=0; i<N; i++
 		//send picture
 		//write picture
-		//send move n
+		//send move L
 		//wait 1
 		//send stop
 		//send GPS
 		//write GPS
-		//send turn 180(n-2)
+		//send turn 180(N-2)
 		//wait 1
 		//send stop
 }
